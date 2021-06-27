bcb066bc9b2e913f01f9cedc45412c6acd1205c7
WdvOps <dvops@outlook.com>
2021-06-26 21:10:12 -0300

    aula de introdução ao react

---
+++ src/RecipeTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles/index.css'
+
+function RecipeTitle() {
+    const title = 'Mashed potatoes';
+    return(
+        <h2>{ title }</h2>
+    )
+};
+
+export default RecipeTitle;
